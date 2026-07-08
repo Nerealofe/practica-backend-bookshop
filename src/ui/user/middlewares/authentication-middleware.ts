30240e2706a4ff41d10b3bf39688fdb3a4753573
Nere L <nerea.lofe@pm.me>
2026-07-08 21:35:31 +0200

    feat: add JWT authentication middleware

---
+++ src/ui/user/middlewares/authentication-middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { SecurityServiceImplementation } from '../../../infrastructure/user/services/SecurityServiceImplementation';
+
+export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json({ error: 'Token not in request' });
+    return;
+  }
+
+  const sanitizedToken = token.replace('Bearer ', '');
+  const securityService = new SecurityServiceImplementation();
+  const decodedToken = securityService.verifyJwt(sanitizedToken);
+
+  if (!decodedToken) {
+    res.status(401).json({ error: 'Token not valid' });
+    return;
+  }
+
+  (req as Request & { userId?: number }).userId = decodedToken.userId;
+
+  next();
+};
