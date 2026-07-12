6bf25725a593d993333e95d25004b84ac3beec1a
Nere L <nerea.lofe@pm.me>
2026-07-12 23:44:04 +0200

    refactor: remove temporary authentication route

--- src/ui/user/routes/user-route.ts
+++ src/ui/user/routes/user-route.ts
@@ -1,13 +1,7 @@
 import { Router } from 'express';
 import { registerUserController } from '../controllers/register-user-controller';
 import { loginUserController } from '../controllers/login-user-controller';
-import { authenticationMiddleware } from '../middlewares/authentication-middleware';
 
 export const userRouter = Router();
 userRouter.post('/signup', registerUserController);
 userRouter.post('/signin', loginUserController);
-userRouter.get('/me', authenticationMiddleware, (req, res) => {
-  res.status(200).json({
-    userId: req.userId,
-  });
-});
